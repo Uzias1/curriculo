81e390f0b8ce80c1c4cabbbf413dda7ef7d2f0cb
Uzias1 <38708130+Uzias1@users.noreply.github.com>
2020-02-19 12:34:12 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Curriculo</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<script type="text/javascript" src="js/archivo.js"></script>
+</head>
+<body>
+	<div class="clase">
+		<header>
+
+		<center><div class ="circulo"><img src="img/Uzías.jpg"></div></center>
+		
+		<nav>
+			<h1>GARCIA LUCERO UZIAS</h1>
+		</nav>
+
+		<!-- aqui ira el fondo xd-->
+	</header>
+	<section>
+		
+		<article>
+			<br> <br>
+			<p>
+				Nombre: Garcia Lucero Uzias
+			</p>
+			<p>
+				Fecha de nacimiento: 10 de Septiembre de 2003
+			</p>
+			<p>
+				Telefono: 55 26552253
+			</p>
+			<p>
+				Email: uzias.garcia.lucero@gmail.com
+			</p>
+		</article>
+		<aside>
+			<div id='division'>
+			<p>
+				Soy un estudiante de programación en el nivel medio superior, actualmente sé programar en JAVA, Python y un diseño de páginas web empleando HTML5 y CSS3.
+				<br> <br>
+				Mi grado de estudios actual se encuentra en el nivel medio superior por el momento sin terminar; sin embargo no pasará mucho tiempo para que concluya mis estudios en este nivel. <br> <br>
+				Me considero capaz para desempeñar cualquier función que se me asigne, ya que aprendo con rápidez, aunque me gusta mas la idea de manejar algun dispositivo o sistema
+				
+			</p>
+			</div>
+		</aside>
+		
+	</section>
+
+	<section>
+		<div id="imagenes">
+			<article>
+				<input type='button' name = 'button' onclick='agregar_contador()' value='Mas sobre mi'><br><br>
+				<center><img id="imagen" src="img/Java.jpg" >
+				<img src="img/trabajo.jpg">
+				<img src="img/experiencia.jpg">
+				<img src="img/idioma.png">
+				<img src="img/ing.jpg"></center>
+			</article>
+		</div>
+	</section>
+
+
+	<footer>
+		<ul id="pie">
+			<li>	
+				<h3><a href="#">García Lucero Uzías</a></h3>
+			</li>
+			<li>
+				<h3><a href="https://www.facebook.com/uzias01">Facebook</a></h3>
+			</li>
+			<li>
+				<h3><a href="https://twitter.com/Uzias78953064">Twitter</a></h3>
+			</li>
+		</ul>
+	</footer>
+	</div>
+	<section class="example2">
+            <ul class="cuadrados">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+	<!-- 
+	.clase{
+	background: #F6F1D1;
+	background: -webkit-linear-gradient(top left, #50a3a2 0%, #53e3a6 100%);
+	background: -moz-linear-gradient(top left, #50a3a2 0%, #53e3a6 100%);
+	background: -o-linear-gradient(top left, #50a3a2 0%, #53e3a6 100%);
+	background: linear-gradient(top left, #50a3a2 0%, #53e3a6 100%);
+
+	position: absolute;
+	top: 50%;
+	left: 0;
+	width: 1005;
+	height: 400px;
+	margin-top: -200px;
+	overflow: hidden;
+}-->
+</body>
+</html>
